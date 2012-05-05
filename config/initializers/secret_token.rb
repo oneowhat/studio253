878b62f47a617dad507c19a3965e58e6967373c2
Brendan Kendrick <brendan@brendankendrick.com>
2012-05-05 11:49:22 -0700

    Generated rails app for studio253 v3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studio253::Application.config.secret_token = '7dc807c65177d217b9940f9a208c88204a10e037aa3396255aaee85fe12aca183e07ff3d83ca7226a53f7b31f3ab0357be95503841924c5a7abcbc7f5ad1b6aa'
